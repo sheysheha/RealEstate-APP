aa1daa3f34b09866c2aef4982760a130f17b1daa
Shehara Kariyawasam <sheharakariyawasam29@gmail.com>
2024-11-28 20:44:55 +0530

    SignUp api route

---
+++ api/routes/AuthRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { signup } from '../controllers/AuthController.js';
+
+const router =express.Router();
+router.post("/signup", signup);
+export default router;
+
